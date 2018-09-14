3c01f8cf2887d5ac85f398673eb436bd53e0e383
developerhincapie <developerhincapie@gmail.com>
2018-09-14 15:14:41 -0500

    get characters in view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CharactersService } from './providers/characters.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  items: any = [];
+
+  constructor(private _charactersService: CharactersService) { }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this._charactersService.getAllCharacters()
+      .subscribe((response: any) => {
+        this.items = response.results;
+        console.log(response);
+      });
+  }
+
 }
